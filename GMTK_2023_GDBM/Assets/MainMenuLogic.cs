5b213c013e0840e2f7b2599870d0d73d570dc2df
germiesqui <germiesqui@gmail.com>
2023-07-08 19:38:55 +0200

    Main Menu

---
+++ GMTK_2023_GDBM/Assets/MainMenuLogic.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class MainMenuLogic : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private TextMeshProUGUI _sliderText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _slider.onValueChanged.AddListener((v) => {
+            _sliderText.text = v.ToString("0");
+            ChangeVolume(v);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame(){
+        SceneManager.LoadScene("MainLevel",LoadSceneMode.Single);
+    }
+
+    public void ChangeVolume(float value){
+        
+    }
+}
