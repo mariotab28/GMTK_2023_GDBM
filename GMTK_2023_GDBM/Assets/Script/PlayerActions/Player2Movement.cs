1c398bb48bd6124637cc6bfc747343b0764b676e
GalacticGarlic <kairaul@hotmail.com>
2023-07-09 17:34:24 +0200

    Added AI behavior

--- GMTK_2023_GDBM/Assets/Script/PlayerActions/Player2Movement.cs
+++ GMTK_2023_GDBM/Assets/Script/PlayerActions/Player2Movement.cs
@@ -5,10 +5,33 @@ using UnityEngine;
 
 public class Player2Movement : PlayerController
 {
+    [Header("AI Settings")]
+    public bool aIMode = false;
+    public float ballFollowSpeed = 5f;
+    public float chanceToFollowHorizontallyPerSecond = 0.02f;
+    public float chanceToGoBackToGoalPerSecond = 0.02f;
+    public float maxHorizontalFollowTime = 5f;
+    public float minHorizontalFollowTime = 2f;
+    public GameObject AIGoal;
     private bool usingGamepad = false;
+    private GameObject followedBall;
+    private bool isFollowingHorizontally = false;
+    private bool isFollowingGoal = false;
 
     // Update is called once per frame
-    void Update() 
+    void Update()
+    {
+        if (aIMode)
+        {
+            AIControls();
+        }
+        else
+        {
+            HumanControls();
+        }
+    }
+
+    private void HumanControls()
     {
         directionX = Input.GetAxisRaw("HorizontalKeyboardPlayer2");
         directionY = Input.GetAxisRaw("VerticalKeyboardPlayer2");
@@ -31,6 +54,102 @@ public class Player2Movement : PlayerController
             kick.Activate();
     }
 
+    private void AIControls()
+    {
+        followedBall = FindClosestBall();
+        bool willFollowHorizontallyThisUpdate = isFollowingHorizontally;
+
+        if (!isFollowingHorizontally)
+        {
+            willFollowHorizontallyThisUpdate = RollChanceToStartFollowingHorizontally();
+            if (willFollowHorizontallyThisUpdate)
+            {
+                isFollowingHorizontally = true;
+                StartCoroutine(TimerUntilStopFollow());
+            }
+            else
+            {
+                bool shouldStartFollowGoal = RollChanceToStartFollowingGoal();
+                if (shouldStartFollowGoal)
+                {
+                    isFollowingGoal = true;
+                    StartCoroutine(TimerUntilStopFollow());
+                }
+            }
+        }
+        FollowTransform(followedBall.transform, willFollowHorizontallyThisUpdate, isFollowingGoal);
+    }
+
+    private IEnumerator TimerUntilStopFollow()
+    {
+        float horizontalFollowSeconds = Random.Range(
+            minHorizontalFollowTime,
+            maxHorizontalFollowTime
+        );
+        yield return new WaitForSeconds(horizontalFollowSeconds);
+        isFollowingHorizontally = false;
+        isFollowingGoal = false;
+    }
+
+    private bool RollChanceToStartFollowingHorizontally()
+    {
+        float roll = Random.Range(0f, 1f);
+        return roll < chanceToFollowHorizontallyPerSecond * Time.deltaTime;
+    }
+
+    private bool RollChanceToStartFollowingGoal()
+    {
+        float roll = Random.Range(0f, 1f);
+        return roll < chanceToGoBackToGoalPerSecond * Time.deltaTime;
+    }
+
+    private void FollowTransform(
+        Transform followedTransform,
+        bool shouldFollowHorizontally,
+        bool shouldPrioritizeGoal
+    )
+    {
+        movement.SetSpeed(ballFollowSpeed);
+        Vector2 direction;
+        if (shouldPrioritizeGoal)
+        {
+            direction = AIGoal.transform.position - transform.position;
+        }
+        else
+        {
+            float verticalDistanceToBall = -(transform.position.y - followedTransform.position.y);
+            float horizontalDirection = shouldFollowHorizontally
+                ? -(transform.position.x - followedTransform.position.x)
+                : 0;
+            direction = new Vector2(horizontalDirection, verticalDistanceToBall).normalized;
+        }
+
+        movement.SetDirection(direction);
+    }
+
+    public GameObject FindClosestBall()
+    {
+        GameObject[] ballsInScene = GameObject.FindGameObjectsWithTag("Ball");
+        return GetGameObjectNearestToTransform(ballsInScene);
+    }
+
+    private GameObject GetGameObjectNearestToTransform(GameObject[] goList)
+    {
+        GameObject[] NearGameobjects = goList;
+        GameObject closestObject = null;
+        float oldDistance = 9999;
+        foreach (GameObject g in NearGameobjects)
+        {
+            float dist = Vector3.Distance(transform.position, g.transform.position);
+            if (dist < oldDistance)
+            {
+                closestObject = g;
+                oldDistance = dist;
+            }
+        }
+        return closestObject;
+    }
+
     private void CheckSprintInput()
     {
         if (Input.GetKeyDown(KeyCode.RightShift))
@@ -50,7 +169,5 @@ public class Player2Movement : PlayerController
 
         if (Input.GetAxisRaw("SprintGamepadPlayer2") <= 0 && usingGamepad)
             movement.SetSpeed(defaultSpeed);
-
     }
-
 }
